f0d0fe760ee399b36e895027ee1fc4e2ba92ee28
Ricardo Olivera <rholivera93@gmail.com>
2022-09-11 22:25:43 -0300

    checkstyle, hooks

--- src/main/java/com/epsilon/homebanking/HomebankingApplication.java
+++ src/main/java/com/epsilon/homebanking/HomebankingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomebankingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomebankingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(HomebankingApplication.class, args);
+  }
 
 }
