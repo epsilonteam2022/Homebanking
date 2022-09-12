72ee74700676e7107b5c6e2c932afd7f49cee087
Ricardo Olivera <rholivera93@gmail.com>
2022-09-12 19:02:49 -0300

    testing checkstyle on github

---
+++ src/main/java/com/epsilon/homebanking/auth/infrastructure/database/entity/UserEntity.java
@@ -0,0 +1,13 @@
+package com.epsilon.homebanking.auth.infrastructure.database.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class UserEntity {
+
+  @Id
+  @GeneratedValue
+                    private Long userId;
+}
