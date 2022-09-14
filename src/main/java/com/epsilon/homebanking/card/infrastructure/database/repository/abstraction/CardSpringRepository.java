d7e5b5d288093e8485a82f1680db089c20c8d106
Ricardo Olivera <rholivera93@gmail.com>
2022-09-14 07:55:19 -0300

    fixing styles

--- src/main/java/com/epsilon/homebanking/card/infrastructure/database/repository/abstraction/CardSpringRepository.java
+++ src/main/java/com/epsilon/homebanking/card/infrastructure/database/repository/abstraction/CardSpringRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CardSpringRepository extends JpaRepository<CardEntity ,Long> {
+public interface CardSpringRepository extends JpaRepository<CardEntity, Long> {
 
 }
