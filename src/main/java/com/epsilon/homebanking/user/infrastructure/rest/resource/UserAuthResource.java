bf21512a7b514b42cf7ebe48037a4c8e005a289d
Ricardo Olivera <rholivera93@gmail.com>
2022-09-26 18:04:38 -0300

    ricky: added crossorigin anotation in auth controller

--- src/main/java/com/epsilon/homebanking/user/infrastructure/rest/resource/UserAuthResource.java
+++ src/main/java/com/epsilon/homebanking/user/infrastructure/rest/resource/UserAuthResource.java
@@ -14,12 +14,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class UserAuthResource {
