95a20e8905721cb58b0121204bd30e630b5440d8
Ricardo Olivera <rholivera93@gmail.com>
2022-09-27 20:42:56 -0300

    ricky : deleted app yml and added app properties for test purposes only

---
+++ src/main/java/com/epsilon/homebanking/user/infrastructure/rest/resource/UserResource.java
@@ -0,0 +1,16 @@
+package com.epsilon.homebanking.user.infrastructure.rest.resource;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public String getFakeUser(){
+    return "Hola, soy un user";
+  }
+}
